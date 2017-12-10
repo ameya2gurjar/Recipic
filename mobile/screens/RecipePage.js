5a563708ab790ee716c5ae443f77296c17a5756e
Aishwarya Malpani <Aishwarya@Aishwaryas-MacBook-Pro-2.local>
2017-12-09 22:47:18 -0600

    trying recipes page

--- mobile/screens/RecipePage.js
+++ mobile/screens/RecipePage.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Button,
   StyleSheet,
   Text,
   View,
+  Image,
 } from 'react-native';
 
 import config from '../config'
@@ -16,16 +17,18 @@ class RecipePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'This is the Recipe Page',
     }
   }
-
-
 
   render() {
     return (
       <View>
-        <Text>message: {this.state.message}</Text>
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 20}}>
+        <Image
+            style={{width: 500, height: 200}}
+            source={{uri: 'https://cbsnews2.cbsistatic.com/hub/i/r/2012/03/30/cc26a85a-d26f-11e2-a43e-02911869d855/thumbnail/620x350/c0a45b5d426bb465b160c6fdbce12124/whopper.jpg'}}
+          />
+       </View>
       </View>
     );
   }
